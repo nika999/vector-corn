678ed9661421067b9f2f125d8a05581051c60a4c
Nika <bokan.veronika@gmail.com>
2018-03-19 21:17:47 +0200

    I have changed a few words

---
+++ src/vector/VectorUse.java
@@ -0,0 +1,17 @@
+package vector;
+
+import java.util.Scanner;
+
+class VectorUse // constructor
+{
+	public static void main (String[] args)
+		{
+		Scanner S = new Scanner(System.in);
+		int x = S.nextInt();
+		int y = S.nextInt();
+			Vector vector = new Vector(x,y);
+			//vector.ShowInfo();
+			System.out.println("Corner is: " +vector.CornerVec());
+			System.out.println("Radious is: " +vector.Radious());
+		}
+} 
